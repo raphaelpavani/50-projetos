97b3cd589135829178b2ca6e58f8eb37d7faf696
raphaelpavani <raphaelpavani@gmail.com>
2021-06-16 13:16:35 -0300

    Projeto 01

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll('.panel');
+panels.forEach((panel)=>{
+  panel.addEventListener('click',()=>{
+    removeActionClasses()
+    panel.classList.add('active')
+  })
+})
+
+function removeActionClasses(){
+  panels.forEach(panel=>{
+    panel.classList.remove('active')
+  })
+}
